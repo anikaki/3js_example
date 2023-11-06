7a38033ef678d0347a49c1665c9f80e03da03bd6
anikaki <112775345+anikaki@users.noreply.github.com>
2023-11-06 11:55:28 -0500

    3d model added to scene

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-// import * as THREE from 'three';
-// import * as THREE from 'three';
-// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-
-// const controls = new OrbitControls( camera, renderer.domElement );
-// const loader = new GLTFLoader();
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 //load the 3d asset 
 
@@ -24,14 +18,19 @@ loader.load( 'family-table.glb', function ( gltf ) {
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
+
+// Lights
+const light = new THREE.AmbientLight( 0xffffff ); // soft white light
+scene.add( light );
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+scene.add( directionalLight );
+
+const controls = new OrbitControls( camera, renderer.domElement );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
 camera.position.z = 5;
 
